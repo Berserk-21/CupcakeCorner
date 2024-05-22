f45676ba73aaac55959e9023ede596a346d4cdef
Berserk-21 <cryptogekkofr@protonmail.com>
2024-05-22 16:40:34 +0200

    Adds an async method to load data from itunes api

--- CupcakeCorner/ContentView.swift
+++ CupcakeCorner/ContentView.swift
@@ -19,6 +19,26 @@ struct ContentView: View {
                 Text(item.collectionName)
             })
         }
+        .task {
+            await loadData()
+        }
+    }
+    
+    /// Gets a list of songs from itunes
+    func loadData() async {
+        
+        guard let url = URL(string: "https://itunes.apple.com/search?term=taylor+swift&entity=song") else {
+            print("invalid url")
+            return
+        }
+        
+        do {
+            let (data, response) = try await URLSession.shared.data(from: url)
+            self.results = try JSONDecoder().decode(Response.self, from: data).results
+            
+        } catch {
+            print("Invalid data")
+        }
     }
 }
 
